95f3ccfe518d11a9205ad3dadee1cf070d61e853
Andrew Aghoghovwia <androyt1@gmail.com>
2022-02-16 14:46:53 +0100

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const navigate=useNavigate()
   return (
     <>
         <header className='w-full px-3 h-[calc(100vh-70px)] flex flex-col justify-center items-center bg-zinc-200'>
@@ -9,10 +11,10 @@ const Home = () => {
             <h3 className='text-xl md:text-3xl mt-4 tracking-wider font-semibold text-slate-800'>Lorem ipsum dolor sit amet</h3>
             <p className='md:w-3/4 mt-4 text-sm tracking-widest text-blue-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempore fuga accusamus amet harum molestiae vel. Vel explicabo libero corrupti asperiores error sed, magni iste commodi nesciunt nobis, perspiciatis perferendis.</p>
 
-            <button className='mt-10 w-2/3 md:w-1/3 bg-blue-600 text-white font-semibold px-3 py-3 rounded-md drop-shadow-lg'>Explore</button>
+            <button className='mt-10 w-2/3 md:w-1/3 bg-blue-600 text-white font-semibold px-3 py-3 rounded-md drop-shadow-lg' onClick={()=>{navigate('/products')}}>Start Shopping</button>
             </div>
         </header>
-    </>
+    </> 
   )
 }
 
