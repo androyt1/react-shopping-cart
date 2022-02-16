545301b7b0637fdfa8c8436ac3e2f06883d36023
Andrew Aghoghovwia <androyt1@gmail.com>
2022-02-16 14:52:10 +0100

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
             <div className='w-full flex flex-col justify-center items-center text-center '>
             <h1 className='text-3xl md:text-5xl lg:text-7xl font-bold text-slate-800 font-serif'>The Clothes Shop</h1>
             <h3 className='text-xl md:text-3xl mt-4 tracking-wider font-semibold text-slate-800'>Lorem ipsum dolor sit amet</h3>
-            <p className='md:w-3/4 mt-4 text-sm tracking-widest text-blue-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempore fuga accusamus amet harum molestiae vel. Vel explicabo libero corrupti asperiores error sed, magni iste commodi nesciunt nobis, perspiciatis perferendis.</p>
+            <p className='md:w-3/4 mt-10 text-sm tracking-widest text-slate-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempore fuga accusamus amet harum molestiae vel. Vel explicabo libero corrupti asperiores error sed, magni iste commodi nesciunt nobis, perspiciatis perferendis.</p>
 
             <button className='mt-10 w-2/3 md:w-1/3 bg-blue-600 text-white font-semibold px-3 py-3 rounded-md drop-shadow-lg' onClick={()=>{navigate('/products')}}>Start Shopping</button>
             </div>
