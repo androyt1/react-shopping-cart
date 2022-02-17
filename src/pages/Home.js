9deda2c0d9fbdc4164ba9bebf67ba6aeb0620e10
Andrew Aghoghovwia <androyt1@gmail.com>
2022-02-17 10:25:19 +0100

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             <h3 className='text-xl md:text-3xl mt-4 tracking-wider font-semibold text-[#030212]'>Lorem ipsum dolor sit amet</h3>
             <p className='md:w-3/4 mt-10 text-sm tracking-widest text-[#0e0e11]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed tempore fuga accusamus amet harum molestiae vel. Vel explicabo libero corrupti asperiores error sed, magni iste commodi nesciunt nobis, perspiciatis perferendis.</p>
 
-            <button className='mt-10 w-2/3 md:w-1/3 bg-[#5bccf6] text-slate-900 font-semibold px-3 py-3 rounded-md drop-shadow-lg' onClick={()=>{navigate('/products')}}>Start Shopping</button>
+            <button className='mt-20 w-2/3 md:w-1/3 border-2 border-zinc-500 shadow-xl hover:text-white font-semibold px-3 py-4 outline-1 rounded-md drop-shadow-lg bg-slate-500 text-slate-50 transition-all duration-700 ease-linear' onClick={()=>{navigate('/products')}}>Start Shopping</button>
             </div>
         </header>
     </> 
